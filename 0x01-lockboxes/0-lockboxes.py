841b8573406e4b1afe1f40a96b767a2674bb4e27
kirubelks <kirubel.n.gebeyehu@gmail.com>
2023-10-07 14:39:34 +0300

    code fix trial 2

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,13 +5,12 @@
 
 def canUnlockAll(boxes):
     '''Checks if all the boxes in a list of boxes containing the keys
-    (inidices) to other boxes can be unlocked given that the first 
-    box is unlocked
+    (indices) to other boxes can be unlocked given that the first
+    box is unlocked.
     '''
-    
     n = len(boxes)
     seen_boxes = set([0])
-    unseen_boxes = set(boxes[0]) .defference(set([0]))
+    unseen_boxes = set(boxes[0]).difference(set([0]))
     while len(unseen_boxes) > 0:
         boxIdx = unseen_boxes.pop()
         if not boxIdx or boxIdx >= n or boxIdx < 0:
@@ -19,4 +18,4 @@ def canUnlockAll(boxes):
         if boxIdx not in seen_boxes:
             unseen_boxes = unseen_boxes.union(boxes[boxIdx])
             seen_boxes.add(boxIdx)
-    return n == len(seen_boxes)        
+    return n == len(seen_boxes)
